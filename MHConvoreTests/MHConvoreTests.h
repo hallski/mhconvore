04b7d643624768c30a4ab624aa40f8c00e9a4701
Mikael Hallendal <mhallendal@gmail.com>
2011-02-16 22:52:54 +0100

    Initial Commit

---
+++ MHConvoreTests/MHConvoreTests.h
@@ -0,0 +1,17 @@
+//
+//  MHConvoreTests.h
+//  MHConvoreTests
+//
+//  Created by Mikael Hallendal on 2011-02-16.
+//  Copyright 2011 Mikael Hallendal. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface MHConvoreTests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ MHConvoreTests/MHConvoreTests.m
@@ -0,0 +1,33 @@
+//
+//  MHConvoreTests.m
+//  MHConvoreTests
+//
+//  Created by Mikael Hallendal on 2011-02-16.
+//  Copyright 2011 Mikael Hallendal. All rights reserved.
+//
+
+#import "MHConvoreTests.h"
+
+
+@implementation MHConvoreTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in MHConvoreTests");
+}
+
+@end
